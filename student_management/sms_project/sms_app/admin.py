4b9b3879b06321763bb836425a4f0547d28c6202
wjh0202 <18706739757@163.com>
2025-04-16 21:51:04 +0800

    Delete student_management directory

--- student_management/sms_project/sms_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import ClassInformation, Student, Course, Score
-
-admin.site.register(ClassInformation)
-admin.site.register(Student)
-admin.site.register(Course)
-admin.site.register(Score)
--- student_management/sms_project/sms_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SmsAppConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "sms_app"
